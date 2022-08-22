7a7e883598392fe80e53bcaa62b9d98ddef75ac3
Sébastien LAURENT <109630378+laforge-tech@users.noreply.github.com>
2022-08-22 22:29:05 +0000

    Fixing log

--- src/bin/kuber.rs
+++ src/bin/kuber.rs
@@ -1,8 +1,10 @@
 
 
+use tracing::level_filters::LevelFilter;
 use tracing_subscriber::fmt;
 use tracing_subscriber::prelude::*;
-use tracing::{ event, Level };
+use tracing::{ event, info , Level };
+
 
 use kube::{ Client, Api, api::ListParams };
 use k8s_openapi::api::core::v1::Pod;
@@ -12,7 +14,7 @@ fn setup_log() {
 
     let sub = tracing_subscriber::registry()
         .with(fmt::Layer::default().with_writer(appender).with_ansi(false))
-        .with(fmt::Layer::default().compact());
+        .with(fmt::Layer::default().compact().with_filter(LevelFilter::INFO));
 
     tracing::subscriber::set_global_default(sub).unwrap();
 }
@@ -23,14 +25,15 @@ async fn main() {
 
     let client = Client::try_default().await.unwrap();
     
-    let pods : Api<Pod> = Api::namespaced(client, "kube-system");
+    let pods : Api<Pod> = Api::all(client);
 
     let lp = ListParams::default();
 
     let lst = pods.list(&lp).await.unwrap();
 
     for p in lst {
-        println!("POD: {}", p.metadata.name.unwrap());
+        //println!("POD: {}", p.metadata.name.unwrap());
+        info!( "POD: {:?}", p.metadata.name.unwrap());
     }
 
     event!(Level::INFO, "working...");
